1361388a67d96eb051ea1afc9865553b87196f09
Azize Zinadinova <azizeshka87@gmail.ru>
2021-11-24 00:51:55 +0300

    Сортировка билетов по цене

---
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -0,0 +1,36 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Ticket;
+import ru.netology.repository.TicketRepository;
+
+public class TicketManager {
+    private TicketRepository repository;
+
+    public TicketManager(TicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addTicket(Ticket item) {
+        repository.save(item);
+    }
+
+    public Ticket[] searchBy(String from, String to) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (matches(ticket, from, to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    private boolean matches(Ticket ticket, String from, String to) {
+        if (ticket.getFrom().contains(from) && ticket.getTo().contains(to)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -0,0 +1,50 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Ticket;
+import ru.netology.repository.TicketRepository;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketManagerTest {
+    TicketRepository repository = new TicketRepository();
+    TicketManager ticketManager = new TicketManager(repository);
+
+    Ticket firstTicket = new Ticket(1, 1299, "SVO", "KZN", 95);
+    Ticket secondTicket = new Ticket(2, 2199, "VKO", "KZN", 95);
+    Ticket thirdTicket = new Ticket(3, 3100, "DME", "KZN", 90);
+
+    @BeforeEach
+    public void setUp() {
+        ticketManager.addTicket(firstTicket);
+        ticketManager.addTicket(secondTicket);
+        ticketManager.addTicket(thirdTicket);
+
+    }
+
+
+
+    @Test
+    void searchByDirection() {
+        Ticket[] actual = ticketManager.searchBy("SVO", "KZN");
+        Ticket[] expected = new Ticket[]{firstTicket};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSortByPrice() {
+        Ticket[] actual = new Ticket[] {thirdTicket, firstTicket, secondTicket};
+        Ticket[] expected = new Ticket[]{firstTicket, secondTicket, thirdTicket};
+
+        Arrays.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+
+    }
