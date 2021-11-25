97e7777c7545b96d503ce840b6c830db773b0213
Azize Zinadinova <azizeshka87@gmail.ru>
2021-11-25 23:00:47 +0300

    добавление  Arrays.sort(result)

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -3,6 +3,8 @@ package ru.netology.manager;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
+import java.util.Arrays;
+
 public class TicketManager {
     private TicketRepository repository;
 
@@ -24,6 +26,8 @@ public class TicketManager {
                 result = tmp;
             }
         }
+
+        Arrays.sort(result);
         return result;
     }
 
